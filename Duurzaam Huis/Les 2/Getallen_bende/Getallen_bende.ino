56fcdaafd470f2729c827721329308325fe99e84
Daan Pronk <daanpronk570@gmail.com>
2025-02-14 12:43:19 +0100

    Les 2

---
+++ Duurzaam Huis/Les 2/Getallen_bende/Getallen_bende.ino
@@ -0,0 +1,45 @@
+void setup() {
+  Serial.begin(115200);
+  pinMode(D6, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D3, OUTPUT);
+
+}
+
+void loop() {
+  int A = random(999);
+  int B = random(999);
+  int C = random(999);
+
+  int D = random(999);
+
+  
+  if (A > B||A < C) {
+      digitalWrite(D3, HIGH);
+    }
+
+  if (C > A && A < C) {
+      digitalWrite(D5, HIGH);
+    }
+
+  if (B > C && B > A && C < A){  
+      digitalWrite(D6, HIGH);
+    }
+
+  if (D > A && D > B && D > C) {
+      digitalWrite(D3, HIGH);
+      digitalWrite(D5, HIGH);
+      digitalWrite(D6, HIGH);
+
+      delay (1000);
+
+      digitalWrite(D3, LOW);
+      digitalWrite(D5, LOW);
+      digitalWrite(D6, LOW);
+
+      delay (1000);
+    }
+
+delay (10000);
+
+}
